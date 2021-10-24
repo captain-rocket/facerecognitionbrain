961ae97b590d265f11447388cb30597565efe5aa
rogerb45365 <59418774+rogerb45365@users.noreply.github.com>
2021-10-24 17:24:50 -0400

    Corrected spelling errors, added bounding box from API applied, data to CSS to draw bounding box & added FaceRecognition.css & properties.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import FaceRocognition from './components/FaceRocognition/FaceRocognition';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 const app = new Clarifai.App({
  apiKey: '4c689cfbfe1c4a27b7faebaa29fef359'
@@ -36,9 +36,30 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+  const image = document.getElementById('inputImage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  console.log(width, height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }  
+}
+
+ displayFaceBox = (box) => {
+   console.log(box);
+   
+   this.setState({box: box})
+ }
 
 onInputChange = (event) => {
   this.setState({input: event.target.value});  
@@ -46,14 +67,11 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input});
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then( function(response) {
-    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    
-  },
-  function(err) {
-    
-  }
-  );
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL, 
+    this.state.input)
+  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err => console.log(err));
 }
 
   render () {
@@ -68,7 +86,7 @@ onButtonSubmit = () => {
         onInputChange={this.onInputChange}
         onButtonSubmit={this.onButtonSubmit}
         className='ImageLinkForm'/>
-        <FaceRocognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
 
       </div>
     );
