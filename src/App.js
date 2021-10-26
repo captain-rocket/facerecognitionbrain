38eb77c0c5e42302eb394c1d9000f759453fbb18
rogerb45365 <59418774+rogerb45365@users.noreply.github.com>
2021-10-26 19:08:54 -0400

    Added isSignIn route & register link to nav.

--- src/App.js
+++ src/App.js
@@ -41,7 +41,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -60,8 +61,6 @@ calculateFaceLocation = (data) => {
 }
 
  displayFaceBox = (box) => {
-   console.log(box);
-   
    this.setState({box: box})
  }
 
@@ -79,6 +78,11 @@ onButtonSubmit = () => {
 }
 
 onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState({isSignedIn: false})
+  } else if (route === 'home') {
+    this.setState({isSignedIn: true})
+  }
   this.setState({route: route})
 } 
 
@@ -87,7 +91,7 @@ onRouteChange = (route) => {
       <div className="App">
         <Particles className='particles'
         params={particlesOptions}/>
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         { this.state.route === 'home' 
         ? <div>
           <Logo />
