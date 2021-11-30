c37c5ba761ae08deb68fcacf8b983996f38f138f
rogerb45365 <59418774+rogerb45365@users.noreply.github.com>
2021-11-30 18:47:05 -0500

    deploying smart-brain-cr

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
   }
 
 onSubmitSignIn = () => {
-  fetch('http://localhost:3000/signin', {
+  fetch('https://agile-coast-50768.herokuapp.com/signin', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
